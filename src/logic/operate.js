66bbf9e3bf478924657886c70e56377981082ee0
david bassey <davidosky1@gmail.com>
2021-05-19 10:00:32 +0100

    Fix Division by zero error

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,33 +1,31 @@
+/* eslint-disable no-return-assign */
 import Big from 'big.js';
 
 const operate = (numberOne, numberTwo, operator) => {
   if (!numberOne || !numberTwo || !operator) return '0';
 
   let result = 0;
-  const num1 = typeof numberOne !== 'number' ? Big(Number(numberOne)) : Big(numberOne);
-  const num2 = typeof numberTwo !== 'number' ? Big(Number(numberTwo)) : Big(numberTwo);
-  if (num1 === 'undefined' || num2 === 'undefined') {
-    result = 'undefined';
-  } else {
-    switch (operator) {
-      case '+':
-        result = num1.plus(num2);
-        break;
-      case 'x':
-        result = num1.times(num2);
-        break;
-      case '-':
-        result = num1.minus(num2);
-        break;
-      case '÷':
-        result = num2 === '0' ? 'undefined' : Big(num1).div(num2).toString();
-        break;
+  // const num1 = typeof numberOne !== 'number' ? Big(Number(numberOne)) : Big(numberOne);
+  // const num2 = typeof numberTwo !== 'number' ? Big(Number(numberTwo)) : Big(numberTwo);
+  switch (operator) {
+    case '+':
+      result = numberOne.plus(numberTwo);
+      break;
+    case 'x':
+      result = numberOne.times(numberTwo);
+      break;
+    case '-':
+      result = numberOne.minus(numberTwo);
+      break;
+    case '÷':
+      result = numberTwo === '0' ? 'undefined' : Big(numberOne).div(numberTwo).toString();
+      break;
 
-      default:
-        result = 0;
-        break;
-    }
+    default:
+      result = 0;
+      break;
   }
+
   return result.toString();
 };
 
